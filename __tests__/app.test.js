42202a1e4c834d7813af31ee2f17626039992bd6
Lexus Banton <lexusbanton.kr@gmail.com>
2022-11-01 15:40:10 -0700

    wrote Zodiac route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,12 @@ const app = require('../lib/app');
 
 const { signs } = require('../lib/signs-data');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
+describe('zodiac route', () => {
+  it('/signs should return list of signs', async () => {
+    const res = await request(app).get('/signs');
+    const expected = signs.map((sign) => {
+      return { id: sign.id, name: sign.name };
+    });
+    expected(res.body).toEqual(expected);
   });
 });
