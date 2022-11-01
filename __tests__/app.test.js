e27c8670c2f18986dbf729ba4c51af38dbd94715
Lexus Banton <lexusbanton.kr@gmail.com>
2022-11-01 15:32:32 -0700

    imported signs

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { signs } = require('../lib/signs-data');
+
 describe('example test - you should probably update me', () => {
   it('home route should return hello world', async () => {
     const resp = await request(app).get('/');
