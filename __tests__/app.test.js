51c537d43c21bbaf4d068ee2766cf448c0b6c0da
Lexus Banton <lexusbanton.kr@gmail.com>
2022-11-01 18:30:29 -0700

    built  id test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,15 @@ describe('signs route', () => {
     });
     expect(res.body).toEqual(expected);
   });
+
+  it('/signs/:id should return sign detail', async () => {
+    const res = await request(app).get('signs/1');
+    const aquarius = {
+      id: '1',
+      name: aquarius,
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    };
+    expect(res.body).toEqual(aquarius);
+  });
 });
